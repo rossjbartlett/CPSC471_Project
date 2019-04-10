45e1dda69c4d9bf66606cfe2d717fc567ddc933b
Ross Bartlett <rossjbartlett@gmail.com>
2019-04-10 16:40:41 -0600

    CMS titile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Company Management System</title>
+    <title>CMS</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
